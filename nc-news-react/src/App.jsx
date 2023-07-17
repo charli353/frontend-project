8c6c5f4af55caf18b164325b86d0cea31999cde4
Charles Heseltine <charlieheseltine@Charless-MacBook-Pro.local>
2023-07-17 13:57:22 +0100

    all articles

--- nc-news-react/src/App.jsx
+++ nc-news-react/src/App.jsx
@@ -1,71 +1,17 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
-import { getArticles } from './api'
 import axios from 'axios'
-
+import Home from './components/Home'
 
 export const defaultURL = axios.create({
   baseURL : 'https://nc-news-production.onrender.com/api'
 })
 
 function App() {
-
-  const [trending, setTrending] = useState(true)
-  const [articles, setArticles] = useState([])
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    getArticles().then((response) => {
-      setArticles(response)
-    })
-    .then(()=>{
-      setLoading(false)
-    })
-  }, [])
-
-  
-
-  return loading ? <p>Loading...</p> : (
-    <main id='gridContainer'>
-      <nav id='nav'>
-        <h1>Home Page</h1>
-      </nav>
-      <section id='trending'>
-        <h1>why wont thsi workd</h1>
-        <ul>
-          <RetrieveArticles trending={trending} articles={articles}/>
-        </ul>
-      </section>
-      <section id='search'>
-
-      </section>
-      <section id='post'>
-
-      </section>
-      
-    </main>
+return (
+  <Routes>
+    <Route path='/' element={<Home /> } />
+  </Routes>
   )
 }
-
-function RetrieveArticles(props) {
-  const trending = props.trending
-  const articles = props.articles
-
-  return articles.map((article) => {
-    console.log(article)
-        return ( 
-         <div>
-          <h2>{article.title}</h2>
-          <p>{article.topic}</p>
-          <p>{article.author}</p>
-         </div>
-            
-            
-       )
-      })
-}
-
-
 export default App
---
+++ nc-news-react/src/components/Home.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { getArticles } from '../api'
+
+export default function () {
+
+    const [trending, setTrending] = useState(true)
+    const [articles, setArticles] = useState([])
+    const [loading, setLoading] = useState(true)
+  
+    useEffect(() => {
+      getArticles().then((response) => {
+        setArticles(response)
+      })
+      .then(()=>{
+        setLoading(false)
+      })
+    }, [])
+
+
+
+    return loading ? <p>Loading...</p> : (
+        <main id='gridContainer'>
+          <nav id='nav'>
+            <h1>NC News</h1>
+          </nav>
+          <section id='trending'>
+            <header>
+                <h2>View Articles</h2>
+            </header>
+            <button className='trendall' onClick={(event) => {
+                setTrending(true)
+            }}>Trending</button>
+            <button className='trendall' onClick={(event) => {
+                setTrending(false)
+            }}>All Articles</button>
+            <ul>
+              <RetrieveArticles trending={trending} articles={articles}/>
+            </ul>
+          </section>
+          <section id='search'>
+    
+          </section>
+          <section id='post'>
+    
+          </section>
+          
+        </main>
+      )
+}
+
+function RetrieveArticles(props) {
+    const trending = props.trending
+    const articles = props.articles
+    if (trending === true) {
+        const filterTrending = articles.filter((article) => {
+                return article.comment_count > 10
+              })
+        return filterTrending.map((article) => {
+
+                return ( 
+                 <div className='article'>
+                  <h2>{article.title}</h2>
+                  <p>Author : {article.author}</p>
+                  <p>Topic : {article.topic}</p>
+                  <p>Comments : {article.comment_count}</p>
+                 </div>
+                    
+                    
+               )
+              })
+    }
+    else {
+        return articles.map((article) => {
+            console.log(article)
+                return ( 
+                 <div className='article'>
+                  <h2>{article.title}</h2>
+                  <p>Author : {article.author}</p>
+                  <p>Topic : {article.topic}</p>
+                 </div>
+                    
+                    
+               )
+              })
+    }
+  
+  }
